a236fc37cf0a8bc9a1e6c2ce159a0d23d125e59c
MichelleMeng <mengxiao@MOMO.local>
2016-12-16 20:35:06 +0800

    commit all

--- SwiftWeather/SwiftWeather_bridging_header.h
+++ SwiftWeather/SwiftWeather_bridging_header.h
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 MichelleMeng. All rights reserved.
 //
 
+#import <AFNetworking/AFNetworking.h>
+
 #ifndef SwiftWeather_bridging_header_h
 #define SwiftWeather_bridging_header_h
 
